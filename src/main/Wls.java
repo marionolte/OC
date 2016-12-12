672c8fa2f413ec12a3823e089556cd82a3a6ab18
SuMario <SuMario@192.168.20.58>
2016-12-12 16:33:04 +0100

    new class

--- src/main/Wls.java
+++ src/main/Wls.java
@@ -13,10 +13,21 @@ public class Wls extends MainTask{
     public Wls(String[] args) {
         super();
         super.prop = parseArgs(args);
+        
+        
     }
     
+    public void wlsTest() {
+        
+    }
+    
+    public void wlsCheckOut(){
+        
+    }
     public static void main(String[] args) {
        Wls w = new Wls(args);   
+           if ( w.isCommand("TEST") ) { w.wlsTest(); }
+           else if ( w.isCommand("CHECKOUT") ) { w.wlsCheckOut(); }
     }
 
     
