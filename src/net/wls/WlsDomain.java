bfd47c3b0f7fa7d07da5bf430ee98a54ef3b95d6
SuMario <SuMario@192.168.20.58>
2016-12-13 12:46:42 +0100

    new class

--- src/net/wls/WlsDomain.java
+++ src/net/wls/WlsDomain.java
@@ -5,17 +5,41 @@
  */
 package net.wls;
 
+import io.file.ReadDir;
+import io.file.XMLReadFile;
+import java.io.File;
+import main.MainTask;
 /**
  *
  * @author SuMario
  */
-public class WlsDomain {
-    
+public class WlsDomain extends MainTask{
+    private ReadDir confdir;
+    private XMLReadFile conf;
     public WlsDomain(String[] args) {
-        
+       super(args);
+       confdir = new ReadDir(".");
+       String te = getProperty("CONF");
+       if (te != null && ! te.isEmpty() ) { confdir=new ReadDir(te); }
+       if ( confdir.isDirectory() ) {
+            conf=new XMLReadFile(confdir.getFQDNDirName()+File.separator+"config"+File.separator+"config.xml");
+            if ( ! conf.isReadableFile() ) {
+                throw new RuntimeException("ERROR: "+conf.getFQDNFileName()+" is not a readable config.xml");
+            }
+                 
+       } else {
+           throw new RuntimeException("ERROR: "+confdir.getFQDNDirName()+" is not a directory ");
+       }
+       
+       
     }
     
     public static void main(String[] args) {
+        WlsDomain wl = getInstance(args);
+    }
+    
+    public static WlsDomain getInstance(String[] args) {
         WlsDomain wl = new WlsDomain(args);
+        return wl;
     }
 }
