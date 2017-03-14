e712c44d868660fa8addf3a9ae536436e434b079
SuMario <SuMario@192.168.20.58>
2017-03-14 10:27:49 +0100

    update

--- src/net/wls/WlsAdminServer.java
+++ src/net/wls/WlsAdminServer.java
@@ -19,10 +19,11 @@ public class WlsAdminServer extends WlsServer {
     private String serverStatControl="console.portal?_nfpb=true&_pageLabel=WLSServerControlTablePage";
     private Http ht=null;
     private boolean _init=false;
-    public WlsAdminServer(Properties prop) throws Exception { super(prop,"WlsAdminServer"); }
-    public WlsAdminServer(HashMap<String,String> nh) throws Exception { super(nh,"WlsAdminServer");  }
     
-    WlsAdminServer(WlsServer ws) { super(ws,"WlsAdminServer"); }
+    public WlsAdminServer(Properties prop) throws Exception { super(prop,"WlsAdminServer"); }
+    public WlsAdminServer(HashMap<String,String> nh,WlsDomain dom) throws Exception { super(nh,"WlsAdminServer",dom);  }
+    
+    WlsAdminServer(WlsServer ws,WlsDomain dom) { super(ws,"WlsAdminServer", dom);  }
     
     private void init(){
       if ( _init ) { return; }  
@@ -32,23 +33,23 @@ public class WlsAdminServer extends WlsServer {
       }catch(Exception e) {}  
     }
     
-    public void connect() {
+    public boolean connect() {
         init();
         try {
             ht.connect();
-            login(); 
-                
-            
+            return login() ;
         } catch(java.io.IOException io) {
             
         }
+        return false;
     }
     
     private boolean login() throws java.io.IOException {
         final String _login="value='Login'";
+        if ( dom.wu == null || dom.wu.getUsername() == null || dom.wu.getPassword() == null ) { return false; }
         if ( ht.verify(_login) ) {
-             ht.setPost("J_username",           wu.getUsername());
-             ht.setPost("j_password",           wu.getPassword());
+             ht.setPost("J_username",           dom.wu.getUsername());
+             ht.setPost("j_password",           dom.wu.getPassword());
              ht.setPost("j_character_encoding", "UTF-8");
              ht.connect(new URL(this.getURIString()+baseAdminUrl+"j_security_check"));
              return ! ht.verify(_login);
@@ -58,11 +59,21 @@ public class WlsAdminServer extends WlsServer {
     
     
     public void getStatus() {
+        final String func=getFunc("getStatus()");
         connect();
         try { 
+             printf(func,1,"like to connect:"+(this.getURIString()+baseAdminUrl+serverStatControl));
              ht.connect(new URL(this.getURIString()+baseAdminUrl+serverStatControl));
              System.out.println("state =>|"+ht.getResponse().toString()+"|<=");
         } catch(java.io.IOException io) {
         }     
     }
+    
+    public String getAdminUrl(){ return this.getURIString(); }
+    
+    public String getAdminStopUrl() {
+        return getAdminUrl().replaceAll("^http", "t3");
+    }
+    
+    public String getAdminServerName(){  return this.getName(); }
 }
