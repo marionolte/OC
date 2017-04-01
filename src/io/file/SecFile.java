404f165a9b7ea63a172d9ce6b7bf2b82bce0c63c
SuMario <SuMario@192.168.20.58>
2017-04-01 19:42:05 +0200

    update

--- src/io/file/SecFile.java
+++ src/io/file/SecFile.java
@@ -5,12 +5,14 @@
  */
 package io.file;
 
-import general.Version;
 import io.crypt.Base64;
 import io.crypt.Crypt;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -101,6 +103,53 @@ public class SecFile extends ReadFile {
         StringBuilder sw = new StringBuilder( crypt.getUnCrypted(super.readOut(begin, end).toString()));
         return sw;
     }
+    
+    @Override
+    public Pattern readPattern() {
+       StringBuilder sw=readOut();
+       StringBuilder sb=new StringBuilder();
+       for ( String line : readOut().toString().split("\n") ) {
+                if ( line != null && ! line.isEmpty() ) {
+                   if ( sb.length() > 0 ) {
+                       sb.append("|");
+                   } 
+                   sb.append(line);
+                }
+       }
+       return Pattern.compile(sb.toString());
+    }
+
+    @Override
+    public String findInFile(String pat) {
+        
+        StringBuilder sw=new StringBuilder();
+        
+        Pattern pa  = Pattern.compile(pat, Pattern.CASE_INSENSITIVE);
+        Matcher ma  = pa.matcher("");
+        try {
+            
+         String line;
+         BufferedReader rb = new java.io.BufferedReader( new java.io.FileReader(filer) );
+         do {
+            line=rb.readLine();
+            if ( line != null && ! line.isEmpty() ) {
+                   ma.reset(line); //reset the input
+                   if (ma.find()) {
+                        sw.append(line.trim()).append("\n");
+                   }                    
+            }
+         } while ( line != null );
+        } catch (Exception e){ }   
+        
+        return sw.toString();
+    }
+
+    @Override
+    public StringBuilder getStringBuilderRef ( ) { return readOut(); }
+
+    @Override
+    public String getString() { return readOut().toString(); }
+
     
     public static void main(String[] args) {
         for(int i=0; i< args.length;i++) {
