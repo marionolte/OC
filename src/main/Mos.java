19d1bff91129ef9e8ef21b1ae0147e22e6f7357a
SuMario <SuMario@192.168.20.58>
2017-04-26 11:16:05 +0200

    update

--- src/main/Mos.java
+++ src/main/Mos.java
@@ -5,9 +5,11 @@
  */
 package main;
 
+import com.trilead.ssh2.SCPClient;
 import io.Console;
 import io.crypt.Crypt;
 import io.file.ReadDir;
+import io.file.SecFile;
 import io.file.WriteFile;
 import io.thread.RunnableT;
 import java.io.File;
@@ -34,6 +36,7 @@ public class Mos extends RunnableT{
     private final Crypt   crypt;
     private final Console console;
     private String[] args=null;
+    private int _exit=-1;
     
     public Mos(String[] args) {
         super();
@@ -44,10 +47,17 @@ public class Mos extends RunnableT{
         
     }
     
-    private void testssl(String ho, String po) { testssl(ho, Integer.parseInt(po)); }
-    private void testssl(String ho, int po   ) { (new TestSSLServer(ho,po)).test(); silent=true; }
+    private boolean testssl(String ho, String po) { return testssl(ho, Integer.parseInt(po)); }
+    private boolean testssl(String ho, int po   ) { 
+            TestSSLServer t = new TestSSLServer(ho,po);
+                          t.test(); 
+                          
+            silent=true; 
+            return  t.isValid();
+    }
     
-    private void ldap(String[] arg) {
+    private boolean ldap(String[] arg) {
+        final String func=getFunc("ldap(String[] arg)");
         silent=true;
         String mod="usage";
         String bindDN=""; String modDN=""; String bindPW=""; String modPW="";
@@ -83,29 +93,29 @@ public class Mos extends RunnableT{
                         // getInstance( String protocol, String hostname, int port, String userDN, String userPWD, String filter , String auth )
                         LdapSearch l = LdapSearch.getInstance( ( "ldap"+((bindSSL)?"s":"")) , bindHost, bindPort, bindDN, bindPW, filter, "");
                         
-                                   l.printResults( l.search(baseDN, filter, objList) );
-
-                    break;
+                                   return l.printResults( l.search(baseDN, filter, objList) );
+                    //break;
 
                 default: 
                     System.out.println("");
-                    return;
+                    return false;
             }
         } catch(RuntimeException | NamingException | IOException ex) {
-            
+            printf(func,1,"LDAPCommand error:"+ex.getMessage());
+            return false;
         }  
     }
     
     private String getPassword(String fn) {
-        WriteFile f = new WriteFile(fn);
+        SecFile f = new SecFile(fn);
         String s = f.readOut().toString();
-        if ( s == null || s.isEmpty() ) { return (String) null;}
+        /*if ( s == null || s.isEmpty() ) { return (String) null;}
         if( s.endsWith("=") ) {
             s=crypt.getUnCrypted(s);
         } else {
             String m=crypt.getCrypted(s);
             f.append(m);
-        }
+        }*/
         return s;
     }
     
@@ -120,35 +130,44 @@ public class Mos extends RunnableT{
         }
         return ar;
     }
+    boolean fin=false;
     private void parseArgs() throws Exception{
         final String func="parseArgs()";
-        boolean fin=false;
+        
         for( int i=0; i<args.length; i++ ) {
-            if      ( args[i].matches("-testssl") ) { testssl(args[++i],args[++i]); fin=true; }
+            if      ( args[i].matches("-testssl") ) { _exit = ( testssl(args[++i],args[++i])     )?0:1;   fin=true; }
             else if ( args[i].matches("-debugssl")) { System.setProperty("javax.net.debug","ssl"); }
-            else if ( args[i].matches("-sshcomm") ) { sshCommand(getArgsLower(args,++i));      fin=true; }
-            else if ( args[i].matches("-ldap")    ) { ldap( getArgsLower(args,++i) ); i=args.length;  fin=true; }
+            else if ( args[i].matches("-sshcomm") ) { _exit = (sshCommand(getArgsLower(args,++i)))?0:1;   fin=true; printf(func,3, "INFO: sshComm parseArgs closed"); }
+            else if ( args[i].matches("-ldap")    ) { _exit = (ldap( getArgsLower(args,++i) )    )?0:1;   fin=true; }
             else if ( args[i].matches("-testhttp")) { String[] ar = getArgsLower(args,++i);
                                                       printf(func,1,"testhttp - start");
+                                                      boolean b=true;
                                                       for (String s: ar) {
-                                                            printf(func,0,"testhttp:"+s);
+                                                            printf(func,2,"testhttp:"+s);
                                                             Http ht= new Http(new URL(s) ); 
                                                                  System.out.println( ht.getResponse().toString());
+                                                                 if( ! b || ht.getResponseCode()<=0 || ht.getResponseCode() > 403 ) { b=false;}      
                                                       }
                                                       printf(func,1,"testhttp - fin");
                                                       fin=true;
+                                                      _exit=(b)?0:1;
                                                     }
             else if ( args[i].matches("-logrotate")){ logRotate(getArgsLower(args,++i));        fin=true; }
             else if ( args[i].matches("-portscan") ){ portScanner(getArgsLower(args,++i));      fin=true; }
             else if ( args[i].matches("-wlsconfig")){ wlsConfigTools(getArgsLower(args,++i),0); fin=true; }
             else if ( args[i].matches("-wlsinfo")  ){ wlsInfoTools(getArgsLower(args,++i));     fin=true; }
-            else if ( args[i].matches("-wlsrota")  ){ wlsRotate(getArgsLower(args,++i)); fin=true;}
-            else if ( args[i].matches("-crypt")    ){ crypt.runArgs(getArgsLower(args,++i));    fin=true; }//  fin=runs("io.crypt.Crypt",getArgsLower(args,i++)); } 
+            else if ( args[i].matches("-wlsrota")  ){ wlsRotate(getArgsLower(args,++i));        fin=true; }
+            else if ( args[i].matches("-logrota")  ){ logApacheRotate(getArgsLower(args,++i));  fin=true; }
+            else if ( args[i].matches("-crypt")    ||
+                      args[i].matches("-uncrypt")  ){ crypt.runArgs(getArgsLower(args,i));      fin=true; }  
+            else if ( args[i].matches("-rota")     ){ logRotate(getArgsLower(args,++i));        fin=true; }
+            else if ( args[i].matches("-gclog")    ){ gcLog(getArgsLower(args,++i));            fin=true; }
             else if ( args[i].matches("-d")        ){ debug++; }
-            else if ( args[i].matches("-version")  ){ version(); }
+            else if ( args[i].matches("-version")  ){ version(); _exit=0; }
             else {
-                usage();
+                usage(); _exit=1;
             }
+            printf(func,4,"parse closed");
             if ( fin ) { setClosed(); return; }
         } 
     }
@@ -161,29 +180,43 @@ public class Mos extends RunnableT{
     }
     
     private void wlsRotate(String[] args) {
+        net.wls.WlsDomainLogRotation.parseArgs(args);
         StringBuilder sw = new StringBuilder();
-        final String sepa="__@@__";
-        for ( int i=0; i< args.length; i++ ) {
-            if        ( args[i].matches("-minsize") ){ WlsDomainLogRotation.minsize  = Long.parseLong(args[++i]); 
-            } else if ( args[i].matches("-minold")  ){ WlsDomainLogRotation.minold   = Integer.parseInt(args[++i]);
-            } else if ( args[i].matches("-maxold")  ){ WlsDomainLogRotation.maxold   = Integer.parseInt(args[++i]);
-            } else if ( args[i].matches("-savefile")){ WlsDomainLogRotation.savefile = args[++i];
-            } else {
-                sw.append(sepa).append(args[i]);
-            }        
-        }
         
-        for ( String s : sw.toString().split(sepa) ) {
+        for ( String s : sw.toString().split(net.wls.WlsDomainLogRotation.sepa) ) {
             if ( ! s.isEmpty() ) {
                 ReadDir di = new ReadDir(s);
-                WlsDomain d = new WlsDomain(di.getDirName());
-                          d.setDomainLocation(di.getFQDNDirName());
-                WlsDomainLogRotation wlog = new WlsDomainLogRotation(d);
-                                     wlog.rotate();
+                if ( di.isDirectory() ) {
+                    WlsDomain d = new WlsDomain(di.getDirName());
+                              d.setDomainLocation(di.getFQDNDirName());
+                    WlsDomainLogRotation wlog = new WlsDomainLogRotation(d);
+                                         wlog.rotate();
+                }                         
             }
         }
         
     }
+    
+    private void logApacheRotate(String[] args) {
+        net.apache.LogRotation.parseArgs(args);
+        
+        for ( String s : net.apache.LogRotation.dirs.split(net.apache.LogRotation.sepa) ) {
+            if ( ! s.isEmpty() ) {
+                ReadDir di = new ReadDir(s);
+                if ( di.isDirectory() ) {
+                net.apache.LogRotation wlog = new net.apache.LogRotation(di);
+                                       wlog.rotate();
+                }                       
+            }
+        }
+        
+    }
+    
+    private void gcLog(String[] args) {
+         org.eclipselabs.garbagecat.Main m = new org.eclipselabs.garbagecat.Main(args);
+                m.scan();
+    }
+    
     
     private void portScanner(String[] args) {
         String min=""; String max = ""; String host="localhost";
@@ -200,10 +233,33 @@ public class Mos extends RunnableT{
         pc.test();
     }
     
-    private void sshCommand(String[] args) {
+    private boolean sshCommand(String[] args) {
+         final String func=getFunc("sshCommand(String[] args)");
+         printf(func,2,"sshCommand start");
+         
+         SSHshell.debug=debug;
          SSHshell ssh = SSHshell.getInstance(args);
-         System.out.println(ssh.sendSingleCommand());
+         if ( ssh.isSSHShell() ) {
+            try { 
+                printf(func,3,"send command :"+ssh.sendSingleCommand().toString());
+                System.out.println(ssh.sendSingleCommand().toString()); 
+            } catch(Exception e) { 
+                printf(func,1,"send command error :"+e.getMessage());
+                return false; 
+            }
+            printf(func,2,"send command return :"+ssh.isValid());
+         } else {
+           try {  
+             SCPClient scp = new SCPClient(ssh.getConnection());
+           } catch (IOException io ) {
+               printf(func,1,"scp command error :"+io.getMessage());
+               return false; 
+           }  
+             
+         }   
+         return ssh.isValid();
     }
+    
     private void wlsInfoTools(String[] args ) {
          WlsUserEnv wue = null;
     
@@ -227,6 +283,8 @@ public class Mos extends RunnableT{
          if ( wue != null ) {
              //System.out.println("dom key:"+domdir+File.separator+"domainkeys"+":   k:"+k+":");
              System.out.println(wue.updateEnv(domdir+File.separator+"domainkeys",k));
+         } else {
+             
          }
     }
     private void wlsConfigTools(String[] args, int j) {
@@ -248,11 +306,18 @@ public class Mos extends RunnableT{
     @Override
     public void run() {
         setRunning();
+        final String func=getFunc("run()");
+        printf(func,4, "INFO: start");
         boolean fail=false;
         if (args != null && args.length > 0 ) {
-           try{ parseArgs(); 
-                fail=true;
-           }catch(Exception e) {}
+           try{ 
+               printf(func,3, "INFO: parseArgs start ");
+               parseArgs(); 
+               fail=true;
+               printf(func,3, "INFO: parseArgs return");
+           }catch(Exception e) {
+               printf(func,1, "ERROR:"+e.getMessage());
+           }
         } else {
             usage();
         }
@@ -260,6 +325,7 @@ public class Mos extends RunnableT{
             // do someting else
         }
         setClosed();
+        printf(func,4, "INFO: closed");
             
     }
     
@@ -267,8 +333,9 @@ public class Mos extends RunnableT{
     public static void main(String[] args) {
            Mos m = new Mos(args); m.silent=true;
                m.start();
-               //while( m.isRunning() ) { sleep(300); }
-               //System.out.println("done.");    
+               while( m.isRunning() && ! m.fin ) { sleep(300); }
+               System.out.println("done."); 
+               System.exit(m._exit);
     }
     
     private void version() {
@@ -287,7 +354,8 @@ public class Mos extends RunnableT{
                 + "\n\t\t-wlsconfig [-dest <script dir [.]>] <domaindir <domaindir1...>>\n\t\t\t\t\t-\tConfigure Wls Starting scripts in directory <dest>\n"
                 + "\n\t\t-wlsinfo <domainhome> [<-server <servername>]\t-\n\t\t\t\t\tprint domain use information\n"
                 + "\n\t\t-wlsrota "+WlsDomainLogRotation.usage()+"\n\t\t\t\t\tweblogic domain logrotation\n"
-                + "\n\t\t-logrotate\t"+(new LogRotation(new String[]{}).usage(false) )
+                + "\n\t\t-logrota "+net.apache.LogRotation.usage()+"\n\t\t\t\t\tapache|ohs logrotation\n"
+                //+ "\n\t\t-logrotate\t"+(new LogRotation(new String[]{}).usage(false) )
                 + "\n\n"
         );
         System.exit(-1);
