43a8a46bfd18cec47386de8d21ca80850cd2b309
mno <mario.nolte@opitz-consulting.com>
2020-11-16 09:50:35 +0100

    update user class

--- src/io/account/User.java
+++ src/io/account/User.java
@@ -15,19 +15,26 @@ abstract public class User {
     private String user;
     private String pass;
     volatile private Crypt crypt=new Crypt();
-    
+
     public User(String user, String pass){
-        
-        this.user=user;
-        this.pass=pass;
+        this.user=(isCrypted(user))?user:getCrypted(user);
+        this.pass=(isCrypted(pass))?pass: crypt.getCrypted(pass);
     }
-    
+    public User(String user, File passFile){
+        this.user=(isCrypted(user))?user:getCrypted(user);
+        setPassword(passFile);
+    }
+
+    public synchronized boolean  isCrypted(String e) { return crypt.isCrypted(e); }
     public synchronized String   getCrypted(String a) { return crypt.getCrypted(a);   }
     public synchronized String getUnCrypted(String a) { return crypt.getUnCrypted(a); }
     public String getUsername() { return getUnCrypted(user); }
     public String getPassword() { return getUnCrypted(pass); }
-    
+
     public void setUsername(String user) { this.user=getUnCrypted(user); }
     public void setPassword(String pass) { this.pass=getUnCrypted(pass); }
-    
+    public void setPassword(File passFile){
+        this.pass=getCrypted( (new SecFile(passFile)).readOut().toString() );
+    }
+
 }
