b79449a0baae7ce5feac7148485a708fb08d2aa2
SuMario <SuMario@192.168.20.58>
2021-01-20 16:59:52 +0100

    update

---
+++ src/comm/oci/iam/User.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comm.oci.iam;
+
+import comm.oci.config.OciConfig;
+
+/**
+ *
+ * @author SuMario
+ */
+public class User extends OciConfig{
+    
+    
+}
